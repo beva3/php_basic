0f8117bc82df9f5e93ca10a1641fb0b3ee3745ef
beva3 <mizabienvenu@gmail.com>
2024-03-11 21:28:21 +0300

    ok

---
+++ getData_post.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>les donne<</title>
+</head>
+<body>
+    <?php echo $_POST["email"]; ?></br>
+</body>
+</html>
